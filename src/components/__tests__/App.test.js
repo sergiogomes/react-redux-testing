53ab266e99d95aa964fff0a0b1a5825b2fb3c1de
sergiogomes <sergiopgomes92@gmail.com>
2020-09-26 22:14:47 -0300

    Importgin enzyme package and writing some tests.

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+
+import App from "../App";
+import CommentBox from "../CommentBox";
+import CommentList from "../CommentList";
+
+it("shows a comment box", () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it("shows a comment list", () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentList).length).toEqual(1);
+});
+
+it("shows the text inside comment box", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  expect(div.innerHTML).toContain("Comment Box");
+  ReactDOM.unmountComponentAtNode(div);
+});
