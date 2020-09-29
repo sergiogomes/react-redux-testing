0ad196e5e94cfb3bfe3badbfc7dfcc27637083c7
sergiogomes <sergiopgomes92@gmail.com>
2020-09-28 21:41:21 -0300

    Simulating formm submition.

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -22,6 +22,16 @@ it("has a text area that users can type in", () => {
   expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
 });
 
+it("has a cleaned text area after form is submited", () => {
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "another comment" },
+  });
+  wrapped.update();
+  wrapped.find("form").simulate("submit");
+  wrapped.update();
+  expect(wrapped.find("textarea").prop("value")).toEqual("");
+});
+
 afterEach(() => {
   wrapped.unmount();
 });
