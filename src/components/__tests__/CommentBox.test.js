6061a2a1f234d85bbd71e0714035e098ef8e2a8b
sergiogomes <sergiopgomes92@gmail.com>
2020-09-28 21:47:48 -0300

    Gruping tests with describe.

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -9,27 +9,28 @@ beforeEach(() => {
   wrapped = mount(<CommentBox />);
 });
 
-it("has a text area and a button", () => {
+it("should have a text area and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
 });
 
-it("has a text area that users can type in", () => {
-  wrapped.find("textarea").simulate("change", {
-    target: { value: "new comment" },
+describe("the text area", () => {
+  beforeEach(() => {
+    wrapped.find("textarea").simulate("change", {
+      target: { value: "new comment" },
+    });
+    wrapped.update();
   });
-  wrapped.update();
-  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
-});
 
-it("has a cleaned text area after form is submited", () => {
-  wrapped.find("textarea").simulate("change", {
-    target: { value: "another comment" },
+  it("should match the text typed in", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
   });
-  wrapped.update();
-  wrapped.find("form").simulate("submit");
-  wrapped.update();
-  expect(wrapped.find("textarea").prop("value")).toEqual("");
+
+  it("should be cleared when the form is submited", () => {
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
 });
 
 afterEach(() => {
