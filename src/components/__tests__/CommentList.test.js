a8612d0d94edcb7413f27c2d20f738f93c0a302a
sergiogomes <sergiopgomes92@gmail.com>
2020-09-30 21:10:43 -0300

    Adding onne test to comment list.

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -5,11 +5,11 @@ import ReduxProvider from "redux-provider";
 import CommentList from "components/CommentList";
 
 let wrapped;
+const initialState = {
+  comments: ["Comment 1", "Comment 2", "Comment 3"],
+};
 
 beforeEach(() => {
-  const initialState = {
-    comments: ["Comment 1", "Comment 2", "Comment 3"],
-  };
   wrapped = mount(
     <ReduxProvider initialState={initialState}>
       <CommentList />
@@ -19,4 +19,9 @@ beforeEach(() => {
 
 it("should create one LI per comment", () => {
   expect(wrapped.find("li").length).toEqual(3);
+});
+
+it("should show the text for each comment", () => {
+  const commentStr = initialState.comments.toString().replace(/,/g, "");
+  expect(wrapped.render().text()).toContain(commentStr);
 });
