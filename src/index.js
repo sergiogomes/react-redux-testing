47612f3280316c029c1cc9300024c7767013fa73
sergiogomes <sergiopgomes92@gmail.com>
2020-10-02 14:20:05 -0300

    Adding react router package.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import ReduxProvider from "redux-provider";
 import App from "components/App";
 
 ReactDOM.render(
   <ReduxProvider>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </ReduxProvider>,
   document.querySelector("#root")
 );
